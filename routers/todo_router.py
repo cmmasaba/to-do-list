1cfe3369bb0dc1efe1499c516c07d1f9896992dc
cmmasaba <mmasabacollins9@gmail.com>
2024-09-03 21:58:11 +0300

    Updated routers

--- routers/todo_router.py
+++ routers/todo_router.py
@@ -1,25 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from controllers.todo_controller import TodoController, TodoCreate, Todo
+from auth.todo_auth import verify_token
 
 router = APIRouter()
 todo_controller = TodoController()
 
-@router.post("/todos", response_model=Todo)
-def create_todo(todo: TodoCreate):
+@router.post("/auth/verify-token")
+async def verify_token_endpoint(decoded_token: dict = Depends(verify_token)):
+    return {"status": "success", "user_id": decoded_token['uid']}
+
+@router.post("/todos", response_model=Todo, dependencies=[Depends(verify_token)])
+async def create_todo(todo: TodoCreate):
     return todo_controller.create_todo(todo)
 
-@router.get("/todos", response_model=list[Todo])
-def get_todo():
+@router.get("/todos", response_model=list[Todo], dependencies=[Depends(verify_token)])
+async def get_todo():
     return todo_controller.get_todos()
 
-@router.get("/todos/{todo_id}", response_model=Todo)
-def get_todo(todo_id: int):
+@router.get("/todos/{todo_id}", response_model=Todo, dependencies=[Depends(verify_token)])
+async def get_todo(todo_id: int):
     return todo_controller.get_todo(todo_id)
 
-@router.put("/todos/{todo_id}", response_model=Todo)
-def update_todo(todo_id: int):
-    return todo_controller.update_todo(todo_id)
+@router.put("/todos/{todo_id}", response_model=Todo, dependencies=[Depends(verify_token)])
+async def update_todo(todo_id: int, updated_todo: TodoCreate):
+    return todo_controller.update_todo(todo_id, updated_todo)
 
-@router.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int):
+@router.delete("/todos/{todo_id}", dependencies=[Depends(verify_token)])
+async def delete_todo(todo_id: int):
     todo_controller.delete_todo(todo_id)
