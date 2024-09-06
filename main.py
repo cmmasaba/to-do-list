abd59c52e7222ea68b7e338a5580e24603236cd6
cmmasaba <mmasabacollins9@gmail.com>
2024-09-06 12:41:35 +0300

    Moved sensitive info to env files

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
+from decouple import Config, RepositoryEnv
 from routers import todo_router
+
+# Get the right environment
+environment = os.getenv("ENVIRONMENT", "development")
+config = Config(RepositoryEnv(f".env.{environment}"))
 
 app = FastAPI()
 app.include_router(todo_router.router)
@@ -10,19 +14,16 @@ app.include_router(todo_router.router)
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[f'{config("ORIGINS")}'],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=[f'{config("METHODS")}'],
     allow_headers=["*"],
 )
 
 # Main block to run the application using Uvicorn server
 if __name__ == "__main__":
     import uvicorn
-    from dotenv import load_dotenv
 
-    load_dotenv()
-
-    HOST = os.getenv("HOST")
-    PORT = os.getenv("PORT")
+    HOST = config("HOST")
+    PORT = config("PORT")
     uvicorn.run("main:app", port=int(PORT), host=HOST, reload=True)
