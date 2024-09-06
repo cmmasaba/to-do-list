be5867ea5143f6e5fd018abdef80360a0b99bc0a
cmmasaba <mmasabacollins9@gmail.com>
2024-09-06 12:43:35 +0300

    using decouple to manage env variables

--- auth/todo_auth.py
+++ auth/todo_auth.py
@@ -3,11 +3,14 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
-from dotenv import load_dotenv
-    
-load_dotenv()
 
-SA_FILE_PATH = os.getenv("SA_FILE_PATH")
+from decouple import Config, RepositoryEnv
+
+# Get the right environment
+environment = os.getenv("ENVIRONMENT", "development")
+config = Config(RepositoryEnv(f".env.{environment}"))
+
+SA_FILE_PATH = config("SA_FILE_PATH")
 
 # initialize the service account
 cred = credentials.Certificate(SA_FILE_PATH)
